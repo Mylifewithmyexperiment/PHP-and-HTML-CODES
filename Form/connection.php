dd53adaded7b1c8d0e32a27902444ffc4b8554b4
SHASHI JAISWAL <shashi.jaiswal17@gmail.com>
2019-01-06 22:40:42 +0530

    php and css codes for creating simple form on web

---
+++ Form/connection.php
@@ -0,0 +1,10 @@
+<?php
+ $con =mysqli_connect('localhost','root','','login') or die(mysqli_error($con));
+
+ //id3687309_jharkhnad -dbname
+ // id3687309_jharkhnad_feed -dbuser
+ // localhost - dbhost
+ 
+ ?>
+
+
